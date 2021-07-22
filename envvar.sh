1d345cb99d016a3bab0416f960504ed51f7d0b94
rojajullapalli <rojajules@gmail.com>
2021-07-22 09:37:34 +0530

    changed enviroment variable program

--- envvar.sh
+++ envvar.sh
@@ -3,6 +3,7 @@ USERSECRET="dH34xJaa23"
 if env | grep USERSECRET=
 then
   echo env variable is already exported
+  echo "local"
 else
   echo env variable was not exported, but now it is
   export USERSECRET
